32caf8d0dbce2d01477f0b0300bdec92cd8a3d68
Clara Moubarak <clara.moubarak@edu.univ-eiffel.fr>
2026-01-14 12:22:08 +0100

    update

--- client/src/features/user/components/UsernamePseudo/index.tsx
+++
@@ -1,1 +0,0 @@
-export { UsernamePseudo } from './UsernamePseudo';
--- client/src/pages/DrawPage.tsx
+++ client/src/pages/DrawPage.tsx
@@ -1,50 +1,47 @@
-import { useState } from 'react';
 import { AppHeader } from '../shared/components/AppHeader/AppHeader'
 import { DrawLayout } from '../shared/components/layouts/DrawLayout/DrawLayout'
+
+/* import { Instructions } from '../shared/components/Instructions/Instructions'
+import { getInstructions } from '../shared/utils/get-instructions' */
 import { UserList } from '../features/user/components/UserList'
 import { DrawArea } from '../features/drawing/components/DrawArea'
 import { useUpdatedUserList } from '../features/user/hooks/useUpdatedUserList'
 import { useJoinMyUser } from '../features/user/hooks/useJoinMyUser'
 import { DrawToolbar } from '../features/drawing/components/DrawToolbar'
-import { UsernamePseudo } from '../features/user/components/UsernamePseudo'
-import { useMyUserStore } from '../features/user/store/useMyUserStore'
 
 function DrawPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const { joinMyUser } = useJoinMyUser();
+  const { joinMyUser }  = useJoinMyUser();
   const { userList } = useUpdatedUserList();
-  const { myUser } = useMyUserStore();
-
-  const handleJoinWithUsername = (username: string) => {
-    joinMyUser(username);
-    setIsModalOpen(false);
-  };
 
   return (
-    <>
-      <UsernamePseudo 
-        isOpen={isModalOpen} 
-        onConfirm={handleJoinWithUsername}
-      />
+    <DrawLayout
+      topArea={<AppHeader 
+        onClickJoin={() => joinMyUser()}
+      />}
+      rightArea={
+        <>
+          {/* <Instructions>
+            {getInstructions('user-list')}
+          </Instructions> */}
+          <UserList users={userList} />
+        </>
+      }
+      bottomArea={
+        <>
+          {/* <Instructions>
+            {getInstructions('toolbar')}
+          </Instructions> */}
+          <DrawToolbar />
+        </>
+      }
+    >
+      <DrawArea />
+      {/* <TestDrawArea /> */}
+      {/* <Instructions className="max-w-xs">
+        {getInstructions('draw-area')}
+      </Instructions> */}
       
-      <DrawLayout
-        topArea={<AppHeader 
-          onClickJoin={() => !myUser ? setIsModalOpen(true) : null}
-        />}
-        rightArea={
-          <>
-            <UserList users={userList} />
-          </>
-        }
-        bottomArea={
-          <>
-            <DrawToolbar />
-          </>
-        }
-      >
-        <DrawArea />
-      </DrawLayout>
-    </>
+    </DrawLayout>
   )
 }
 
